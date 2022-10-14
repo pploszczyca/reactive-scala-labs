753391f77f19ec63b003224c089444464c138321
pploszczyca <piotrus.plo@gmail.com>
2022-10-14 20:41:25 +0200

    Added simple main

---
+++ src/main/scala/EShop/lab2/EShopMain.scala
@@ -0,0 +1,29 @@
+package EShop.lab2
+
+import EShop.lab2.CartActor.{AddItem, RemoveItem}
+import EShop.lab2.Checkout.{ConfirmPaymentReceived, SelectDeliveryMethod, StartCheckout}
+import EShop.lab2.TypedCheckout.SelectPayment
+import akka.actor.{ActorSystem, Props}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+
+object EShopMain {
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("EShop")
+
+    val cart = system.actorOf(Props[CartActor], "cart")
+    cart ! AddItem("T-Shirt")
+    cart ! AddItem("Jeans")
+    cart ! RemoveItem("T-Shirt")
+
+    val checkout = system.actorOf(Props[Checkout], "checkout")
+    checkout ! StartCheckout
+    checkout ! SelectDeliveryMethod("Paczkomat")
+    checkout ! SelectPayment("BLIK")
+    checkout ! ConfirmPaymentReceived
+
+    Await.result(system.whenTerminated, Duration.Inf)
+  }
+}
