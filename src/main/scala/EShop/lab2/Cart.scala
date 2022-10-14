e89abb30d0613f1d13b63410d0e43e565d0875c9
pploszczyca <piotrus.plo@gmail.com>
2022-10-14 17:03:47 +0200

    Implemented CartActor

--- src/main/scala/EShop/lab2/Cart.scala
+++ src/main/scala/EShop/lab2/Cart.scala
@@ -1,12 +1,12 @@
 package EShop.lab2
 
-case class Cart(items: Seq[Any]) {
+case class Cart(private val items: Seq[Any]) {
   def contains(item: Any): Boolean = items.contains(item)
   def addItem(item: Any): Cart     = Cart(items :+ item)
-  def removeItem(item: Any): Cart  = Cart(items.filterNot(item.equals))
+  def removeItem(item: Any): Cart  = Cart(items diff Seq(item))
   def size: Int                    = items.size
 }
 
 object Cart {
-  def empty: Cart = Cart(List())
+  def empty: Cart = Cart(Seq())
 }
--- src/main/scala/EShop/lab2/CartActor.scala
+++ src/main/scala/EShop/lab2/CartActor.scala
@@ -3,20 +3,28 @@ package EShop.lab2
 import akka.actor.{Actor, ActorRef, Cancellable, Props}
 import akka.event.{Logging, LoggingReceive}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
 object CartActor {
 
   sealed trait Command
-  case class AddItem(item: Any)        extends Command
-  case class RemoveItem(item: Any)     extends Command
-  case object ExpireCart               extends Command
-  case object StartCheckout            extends Command
+
+  case class AddItem(item: Any) extends Command
+
+  case class RemoveItem(item: Any) extends Command
+
+  case object ExpireCart extends Command
+
+  case object StartCheckout extends Command
+
   case object ConfirmCheckoutCancelled extends Command
-  case object ConfirmCheckoutClosed    extends Command
+
+  case object ConfirmCheckoutClosed extends Command
 
   sealed trait Event
+
   case class CheckoutStarted(checkoutRef: ActorRef) extends Event
 
   def props = Props(new CartActor())
@@ -24,19 +32,78 @@ object CartActor {
 
 class CartActor extends Actor {
 
+  val system = akka.actor.ActorSystem("system")
+
   import CartActor._
 
-  private val log       = Logging(context.system, this)
-  val cartTimerDuration = 5 seconds
+  private val log = Logging(context.system, this)
+  val cartTimerDuration: FiniteDuration = 5 seconds
 
-  private def scheduleTimer: Cancellable = ???
+  private def scheduleTimer: Cancellable =
+    system.scheduler.scheduleOnce(delay = cartTimerDuration) {
+      self ! ExpireCart
+    }
 
-  def receive: Receive = ???
+  def receive: Receive = empty
 
-  def empty: Receive = ???
+  def empty: Receive = LoggingReceive {
+    case AddItem(item) =>
+      context become nonEmpty(
+        cart = Cart.empty addItem item,
+        timer = scheduleTimer,
+      )
+  }
 
-  def nonEmpty(cart: Cart, timer: Cancellable): Receive = ???
+  def nonEmpty(cart: Cart, timer: Cancellable): Receive = LoggingReceive {
+    case AddItem(item) =>
+      timer.cancel()
+      context become nonEmpty(
+        cart = cart addItem item,
+        timer = scheduleTimer,
+      )
 
-  def inCheckout(cart: Cart): Receive = ???
+    case RemoveItem(item) =>
+      onRemoveItem(
+        cart = cart,
+        item = item,
+        timer = timer,
+      )
+
+    case ExpireCart =>
+      context become empty
+
+    case StartCheckout =>
+      context become inCheckout(cart = cart)
+  }
+
+  private def onRemoveItem(cart: Cart,
+                           item: Any,
+                           timer: Cancellable): Unit = {
+    val newCart = cart removeItem item
+
+    if (isElementRemoved) {
+      timer.cancel()
+      newCart.size match {
+        case size if size > 0 => context become nonEmpty(
+          cart = newCart,
+          timer = scheduleTimer,
+        )
+        case size if size == 0 => context become empty
+      }
+    }
+
+    def isElementRemoved = cart.size != newCart.size
+  }
+
+  def inCheckout(cart: Cart): Receive = LoggingReceive {
+    case ConfirmCheckoutCancelled =>
+      context become nonEmpty(
+        cart = cart,
+        timer = scheduleTimer,
+      )
+
+    case ConfirmCheckoutClosed =>
+      context become empty
+  }
 
 }
