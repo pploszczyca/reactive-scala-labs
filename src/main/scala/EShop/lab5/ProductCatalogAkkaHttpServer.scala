0c715706df7f7af0c81ab271b327f85ebb23045c
pploszczyca <piotrus.plo@gmail.com>
2022-11-17 14:21:49 +0100

    Wrote ProductCatalogAkkaHttpServer

--- src/main/scala/EShop/lab5/ProductCatalogAkkaHttpServer.scala
+++ src/main/scala/EShop/lab5/ProductCatalogAkkaHttpServer.scala
@@ -1,9 +1,8 @@
 package EShop.lab5
 
-import EShop.lab5.ProductCatalogApp.config
 import akka.Done
 import akka.actor.typed.receptionist.Receptionist
-import akka.actor.typed.scaladsl.AskPattern.{Askable, schedulerFromActorSystem}
+import akka.actor.typed.scaladsl.AskPattern.{schedulerFromActorSystem, Askable}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.http.scaladsl.Http
@@ -11,7 +10,6 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.util.Timeout
-import com.typesafe.config.ConfigFactory
 import spray.json.{DefaultJsonProtocol, JsString, JsValue, JsonFormat, RootJsonFormat}
 
 import java.net.URI
@@ -19,49 +17,60 @@ import scala.concurrent.duration.{Duration, DurationInt}
 import scala.concurrent.{Await, Future}
 
 object ProductCatalogAkkaHttpServer {
-  sealed trait Command
-  case class Listing(listing: Receptionist.Listing) extends Command
-
   case class SearchRequest(brand: String, productKeyWords: List[String])
   case class SearchResult(items: List[ProductCatalog.Item])
 }
 
 trait ProductCatalogJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val searchRequest: RootJsonFormat[ProductCatalogAkkaHttpServer.SearchRequest] = jsonFormat2(
-    ProductCatalogAkkaHttpServer.SearchRequest
-  )
-  implicit val searchResult: RootJsonFormat[ProductCatalogAkkaHttpServer.SearchResult] = jsonFormat1(
-    ProductCatalogAkkaHttpServer.SearchResult
-  )
-  implicit val item: RootJsonFormat[ProductCatalog.Item] = jsonFormat5(
-    ProductCatalog.Item
-  )
 
   //custom formatter just for example
   implicit val uriFormat: JsonFormat[URI] = new JsonFormat[java.net.URI] {
     override def write(obj: java.net.URI): spray.json.JsValue = JsString(obj.toString)
+
     override def read(json: JsValue): URI =
       json match {
         case JsString(url) => new URI(url)
-        case _             => throw new RuntimeException("Parsing exception")
+        case _ => throw new RuntimeException("Parsing exception")
       }
   }
+
+  implicit val searchRequest: RootJsonFormat[ProductCatalogAkkaHttpServer.SearchRequest] = jsonFormat2(
+    ProductCatalogAkkaHttpServer.SearchRequest
+  )
+  implicit val item: RootJsonFormat[ProductCatalog.Item] = jsonFormat5(
+    ProductCatalog.Item
+  )
+  implicit val searchResult: RootJsonFormat[ProductCatalogAkkaHttpServer.SearchResult] = jsonFormat1(
+    ProductCatalogAkkaHttpServer.SearchResult
+  )
 }
 
 class ProductCatalogAkkaHttpServer extends ProductCatalogJsonSupport {
-  private implicit val system: ActorSystem[Nothing] = ActorSystem[Nothing](Behaviors.empty, "ProductCatalogHttp")
+  private implicit val system: ActorSystem[Nothing] = ActorSystem[Nothing](Behaviors.empty, "ProductCatalog")
 
   implicit val timeout: Timeout = 3.seconds
 
-  def routes(productCatalogRef: ActorRef[ProductCatalog.Query]): Route = {
+  def routes(): Route = {
     path("search") {
       get {
         entity(as[ProductCatalogAkkaHttpServer.SearchRequest]) { searchRequest =>
-          val response = productCatalogRef
-            .ask(ref => ProductCatalog.GetItems(searchRequest.brand, searchRequest.productKeyWords, ref))
+          val listings: Future[Receptionist.Listing] = system.receptionist
+            .ask((ref: ActorRef[Receptionist.Listing]) =>
+              Receptionist.Find(ProductCatalog.ProductCatalogServiceKey, ref)
+            )
 
-          onSuccess(response) {
-            case ProductCatalog.Items(items) => complete(ProductCatalogAkkaHttpServer.SearchResult(items))
+          val listingResult: Receptionist.Listing = Await.result(listings, Duration.Inf)
+
+          val items: Future[ProductCatalog.Ack] = listingResult
+            .allServiceInstances(ProductCatalog.ProductCatalogServiceKey)
+            .head
+            .ask((ref: ActorRef[ProductCatalog.Ack]) =>
+              ProductCatalog.GetItems(searchRequest.brand, searchRequest.productKeyWords, ref)
+            )
+
+          onSuccess(items) {
+            case ProductCatalog.Items(items) =>
+              complete(ProductCatalogAkkaHttpServer.SearchResult(items))
           }
         }
       }
@@ -69,24 +78,13 @@ class ProductCatalogAkkaHttpServer extends ProductCatalogJsonSupport {
   }
 
   def start(port: Int): Future[Done] = {
-    Behaviors.setup[ProductCatalogAkkaHttpServer.Command] { context =>
-      val listingAdapter = context.messageAdapter[Receptionist.Listing](ProductCatalogAkkaHttpServer.Listing.apply)
-
-      context.system.receptionist ! Receptionist.Find(ProductCatalog.ProductCatalogServiceKey, listingAdapter)
-
-      Behaviors.receiveMessagePartial {
-        case ProductCatalogAkkaHttpServer.Listing(ProductCatalog.ProductCatalogServiceKey.Listing(listing)) =>
-          Http()
-            .newServerAt("localhost", port)
-            .bind(routes(productCatalogRef = listing.head))
-          Behaviors.ignore
-      }
-    }
-
+    Http()
+      .newServerAt("localhost", port)
+      .bind(routes())
     Await.ready(system.whenTerminated, Duration.Inf)
   }
 }
 
 object ProductCatalogAkkaHttpServerApp extends App {
-  new ProductCatalogAkkaHttpServer().start(10000)
+  new ProductCatalogAkkaHttpServer().start(10001)
 }
